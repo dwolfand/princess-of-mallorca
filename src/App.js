7925ac9bbc27f521fb52a98ddab6f568ac7acc84
David Wolfand <david@unitedincome.com>
2018-12-09 00:38:28 -0500

    adding countdown

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Countdown from './Countdown'
 import './App.css';
 
 class App extends Component {
@@ -6,6 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
+          <Countdown date={`2019-09-20T00:00:00`} />
           <img src="/princess-of-mallorca.jpg" className="main-img" alt="The Princess of Mallorca" />
         </header>
       </div>
