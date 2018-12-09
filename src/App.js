64e5c4fb129f758eea2bf8c600731f883f9dd669
David Wolfand <david@unitedincome.com>
2018-12-08 23:51:53 -0500

    Init react and static homepage

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src="/princess-of-mallorca.jpg" className="main-img" alt="The Princess of Mallorca" />
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
